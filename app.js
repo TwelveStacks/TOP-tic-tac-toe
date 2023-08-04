016dbe1e029b360218f7714296f4818c745dd35a
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-08-04 16:38:07 -0400

    Created page layout and tic tac toe grid

---
+++ app.js
@@ -0,0 +1,13 @@
+// Player
+
+// const player = {
+//     playerName: pName,
+//     playerScore: pScore,
+//     playerSymbol: pSymbol
+// }
+
+// const gameBoard = {
+//     firstRow: ["X", "O", "X"],
+//     secondRow: ["X", "O", "X"],
+//     thirdRow: ["X", "O", "X"]
+// }
